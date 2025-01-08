35ce3f425e5d73e6ea7931ca7978384ac98d3a2a
otolba <omar16tolba@gmail.com>
2025-01-08 15:09:13 -0800

    Asteroids spawn and head to ship

--- Assets/Scripts/AsteroidScript.cs
+++ Assets/Scripts/AsteroidScript.cs
@@ -2,7 +2,16 @@ using UnityEngine;
 
 public class AsteroidScript : MonoBehaviour
 {
+    public GameObject ship;
     private float lifetime;
+    private float xTarget; // Stores the randomized x position
+    private float yTarget; // Stores the randomized y position
+    private float zTarget; // Stores the randomized z position
+    
+    void Awake(){
+
+    }
+    
     void Update()
     {
         lifetime += Time.deltaTime;
@@ -12,5 +21,15 @@ public class AsteroidScript : MonoBehaviour
             gameObject.SetActive(false);
             
         }
+
+         // Get the ship's x position and add a random offset
+        xTarget = (ship.transform.position.x - transform.position.x) + Random.Range(-30f, 31f); 
+        // Get the ship's y position and add a random offset
+        yTarget = (ship.transform.position.y - transform.position.y) + Random.Range(-30f, 31f); 
+        // Get the ship's z position and add a random offset
+        zTarget = (ship.transform.position.z - transform.position.z) + Random.Range(-30f, 31f); 
+        
+        Vector3 targetPosition = new Vector3(xTarget, yTarget, zTarget);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, 250*Time.deltaTime); 
     }
 }
--- Assets/Scripts/asteroidMovement.cs
+++ Assets/Scripts/asteroidMovement.cs
@@ -2,42 +2,25 @@ using UnityEngine;
 
 public class AsteroidMovement : MonoBehaviour
 {
-    public Transform target; // Target the asteroid will move toward
-    [SerializeField] private GameObject ship; // Reference to the ship GameObject
-   
-    private float xTarget; // Stores the randomized x position
-    private float yTarget; // Stores the randomized y position
-    private float zTarget; // Stores the randomized z position
+    public GameObject[] asteroidArr;
+    private GameObject currentAsteroid;
 
     
     void Awake()
     {
-        //Randomizes the asteroids spawn position
-        transform.position = new Vector3(Random.Range(-500f, 501f), Random.Range(-500f, 501f), Random.Range(-500f, 501f));
 
-        // Get the ship's x position and add a random offset
-        xTarget = (ship.transform.position.x - transform.position.x) + Random.Range(-30f, 31f); 
-        // Get the ship's y position and add a random offset
-        yTarget = (ship.transform.position.y - transform.position.y) + Random.Range(-30f, 31f); 
-        // Get the ship's z position and add a random offset
-        zTarget = (ship.transform.position.z - transform.position.z) + Random.Range(-30f, 31f); 
     }
 
     void Update()
     {
-        // Ensure the ship reference is valid
-        if (ship != null)
-        {
-            // Move the asteroid toward the randomized Target
-            Vector3 newPosition = transform.position;
-            newPosition.x = Mathf.Lerp(newPosition.x, xTarget, Time.deltaTime * 0.5f);
-            newPosition.y = Mathf.Lerp(newPosition.y, yTarget, Time.deltaTime * 0.5f);
-            newPosition.z = Mathf.Lerp(newPosition.z, zTarget, Time.deltaTime * 0.5f);
-            transform.position = newPosition;
+        for(int n=0; n < asteroidArr.Length; n++){
+            if(!asteroidArr[n].activeInHierarchy){
+                asteroidArr[n].SetActive(true);
+                asteroidArr[n].transform.position = new Vector3(Random.Range(-500f, 501f), Random.Range(-500f, 501f), Random.Range(-500f, 501f));
+                currentAsteroid = asteroidArr[n];
+                break;
+            }
         }
-        else
-        {
-            Debug.LogWarning("Ship GameObject is not assigned!");
-        }
+       
     }
 }
