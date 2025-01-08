40f8a3648b7dcbe82f092486feec9ef9c0954ee2
fraser-barnes <fraserbarnes07@gmail.com>
2025-01-08 15:31:23 -0800

    AI

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -3,27 +3,32 @@ using UnityEngine;
 public class AI : MonoBehaviour
 {
     AudioSource audioSource;
-    [SerializeField]
     AudioClip[] clips;
+    float[] spectrum = new float[128];
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         transform.localScale = new Vector3(1.01f, 0.01f, 1.01f);
-        Talk(clips[0]);
     }
 
-    void Talk(AudioClip clip)
+    void Update()
+    {
+        audioSource.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
+        if (spectrum != null && spectrum.Length > 0)
+        {
+            float l = Mathf.Pow(spectrum[0] * 30,0.4f) + 0.01f;
+            if (l <= 1f)
+                transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(1.01f, l, 1.01f), Time.deltaTime * 10f);
+            else
+                transform.localScale = new Vector3(1.01f, 1f, 1.01f);
+        }
+    }
+
+    public void Talk(AudioClip clip)
     {
         audioSource.clip = clip;
         audioSource.Play();
-        float[] spectrum = new float[256];
-        audioSource.GetSpectrumData(spectrum, 0, FFTWindow.Hanning);
-        foreach (float v in spectrum)
-        {
-            Debug.Log("volume: " + v);
-        }
-
     }
 }
