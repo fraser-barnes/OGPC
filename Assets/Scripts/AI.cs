b44d743c6b4af19385dc006415bc77a2cc549151
fraser-barnes <fraserbarnes07@gmail.com>
2025-01-14 15:32:12 -0800

    ai animation

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -12,6 +12,10 @@ public class AI : MonoBehaviour
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        for (int i = 0; i < circles.Length; i++)
+        {
+            circles[i].localPosition = Vector3.zero;
+        }
     }
 
     void Update()
@@ -21,12 +25,16 @@ public class AI : MonoBehaviour
         {
             if (spectrum != null && spectrum.Length > 0)
             {
-                float l = Mathf.Pow(spectrum[(i + 1) * 6] * 400,0.2f) + 1.01f;
-                if (l <= 5f)
+                float l = Mathf.Pow(spectrum[(i + 1) * 5] * 400,0.25f) + 1.01f;
+                if (l <= 3f)
                     circles[i].localScale = Vector3.Lerp(circles[i].localScale, new Vector3(l, 0.015f, l), Time.deltaTime * 30f);
                 else
                     circles[i].localScale = new Vector3(1.01f, 0.015f, 1.01f);
             }
+            if (audioSource.isPlaying)
+                circles[i].localPosition = Vector3.Lerp(circles[i].localPosition, new Vector3(0f, (i-10)/10f, 0f), Time.deltaTime * 20f);
+            else
+                circles[i].localPosition = Vector3.Lerp(circles[i].localPosition, Vector3.zero, Time.deltaTime * 10f);
         }
 
     }
