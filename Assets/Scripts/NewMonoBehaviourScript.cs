74498757f26e3bd5da51edcb321ab122795507cb
AidanBOrger35 <aidan.g.schwartz@icloud.com>
2025-01-14 14:25:22 -0800

    ShipCollision and ShipHealth

---
+++ Assets/Scripts/NewMonoBehaviourScript.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class shipCollision : MonoBehaviour
+{
+   public ShipHealth shipHealth; // Reference to the ShipHealth script
+   public float damageAmount = 20f; // Damage caused by the asteroid
+
+   private void OnCollisionEnter(Collision collision)
+   {
+       // Check if the collision is with an asteroid
+       if (collision.gameObject.CompareTag("Asteroid"))
+       {
+           // Call the TakeDamage method on the ShipHealth script
+           if (shipHealth != null)
+           {
+               shipHealth.TakeDamage(damageAmount);
+           }
+
+           // Optionally, destroy the asteroid
+           Destroy(collision.gameObject);
+       }
+   }
+
+   private void OnTriggerEnter(Collider other)
+   {
+       // Alternatively, handle trigger collisions if using trigger colliders
+       if (other.CompareTag("Asteroid"))
+       {
+           if (shipHealth != null)
+           {
+               shipHealth.TakeDamage(damageAmount);
+           }
+
+           Destroy(other.gameObject);
+       }
+   }
+}
