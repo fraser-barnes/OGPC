b5dff6ab154cd9720206c7c479d3628e83cf7dfa
otolba <omar16tolba@gmail.com>
2025-01-16 14:34:10 -0800

    asteroid updates

--- Assets/Scripts/asteroidMovement.cs
+++ Assets/Scripts/asteroidMovement.cs
@@ -15,7 +15,7 @@ public class AsteroidMovement : MonoBehaviour
     {
         asteroidCooldown += Time.deltaTime;
         for(int n=0; n < asteroidArr.Length; n++){
-            if(!asteroidArr[n].activeInHierarchy && asteroidCooldown > (5f/11f)){
+            if(!asteroidArr[n].activeInHierarchy && asteroidCooldown > (20f/11f)){
                 asteroidCooldown = 0;
                 asteroidArr[n].transform.position = new Vector3(Random.Range(transform.position.x-3000, transform.position.x+3001), Random.Range(transform.position.y-3000f, transform.position.y+3001f), Random.Range(transform.position.z+5000, transform.position.z+7001));
                 asteroidArr[n].SetActive(true);
