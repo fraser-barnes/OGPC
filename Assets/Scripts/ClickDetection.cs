c8ff90b01b2a5668e198c24b904919b3d24079fe
otolba <omar16tolba@gmail.com>
2025-04-10 15:27:20 -0700

    u

--- Assets/Scripts/ClickDetection.cs
+++ Assets/Scripts/ClickDetection.cs
@@ -23,6 +23,9 @@ public class ClickDetection : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
+
+                Debug.Log("Clicked object: " + hit.transform.gameObject.name)
+
                 if (hit.transform.gameObject.CompareTag("ShieldsSwitch") && !shieldsActive)
                 {
                     shieldsActive = true;
