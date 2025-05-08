28bab9324e2e586844dfe3ac4af9f11959524c5a
fraser-barnes <fraserbarnes07@gmail.com>
2025-05-08 15:25:35 -0700

    titlescreen stuff

--- Assets/Scripts/Start_Game.cs
+++ Assets/Scripts/Start_Game.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+
 public class Start_Game : MonoBehaviour
 {
     public string levelName;
+
+    private void Start() {
+        RenderSettings.skybox.SetColor("_Tint", new Color(0.4f,0.4f,0.4f,1f));
+    }
 
     public void WaitThenLoad() {
         StartCoroutine(LoadLevel());
     }
 
-    IEnumerator LoadLevel(){
+    IEnumerator LoadLevel() {
+        yield return new WaitForSeconds(5);
         SceneManager.LoadScene(levelName);
     }
 }
